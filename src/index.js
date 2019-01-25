fcd28971a6af2fd3bf017e4dc0d064323ff64fb9
MaxLarue <maximilienlarue@gmail.com>
2019-01-25 14:20:28 +0100

    testing

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { hydrate, render } from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
+
 const rootElement = document.getElementById('root');
 if (rootElement.hasChildNodes()) {
   hydrate(<App />, rootElement);
